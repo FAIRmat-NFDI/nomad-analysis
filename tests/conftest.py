5913a4a9a7717d6d50028885204cd1bd29dbcef2
Sarthak Kapoor <sarthakkapoor@hotmail.com>
2024-03-05 13:51:59 +0100

    Define extract_from_logger fixture

---
+++ tests/conftest.py
@@ -0,0 +1,21 @@
+import json
+
+import pytest
+from nomad.utils import structlogging
+
+
+@pytest.fixture(scope='function')
+def extract_from_logger(caplog):
+    '''
+    Extracts log messages from the logger and raises an assertion error if any
+    ERROR messages are found.
+    '''
+    caplog.handler.formatter = structlogging.ConsoleFormatter()
+    yield caplog
+    for record in caplog.get_records(when='call'):
+        if record.levelname in ['ERROR']:
+            try:
+                msg = structlogging.ConsoleFormatter.serialize(json.loads(record.msg))
+            except Exception:
+                msg = record.msg
+            assert False, msg
--- tests/test_schema.py
+++ tests/test_schema.py
@@ -3,7 +3,7 @@ import os.path
 from nomad.client import normalize_all, parse
 
 
-def test_schema():
+def test_schema(extract_from_logger):
     test_file = os.path.join(os.path.dirname(__file__), 'data', 'test.archive.yaml')
     entry_archive = parse(test_file)[0]
     normalize_all(entry_archive)
