a52f89b821031473f5139dd6019eefdec18ee9b3
Sarthak Kapoor <sarthakkapoor@hotmail.com>
2024-03-06 14:21:27 +0100

    Add clean_up fixture

--- tests/test_schema.py
+++ tests/test_schema.py
@@ -3,7 +3,7 @@ import os.path
 from nomad.client import normalize_all, parse
 
 
-def test_schema(extract_from_logger):
+def test_schema(capture_from_logger, clean_up):
     test_file = os.path.join(os.path.dirname(__file__), 'data', 'test.archive.yaml')
     entry_archive = parse(test_file)[0]
     normalize_all(entry_archive)
