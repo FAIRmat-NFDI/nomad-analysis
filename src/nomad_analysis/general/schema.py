fb4e3fa7ed4a5044835fa496879ea3b71a3a9f31
Sarthak Kapoor <sarthakkapoor@hotmail.com>
2024-09-19 14:30:37 +0200

    Add general class for AnalysisResult and AnalysisStep

--- src/nomad_analysis/general/schema.py
+++ src/nomad_analysis/general/schema.py
@@ -16,11 +16,56 @@
 # limitations under the License.
 #
 
+from nomad.datamodel.metainfo.annotations import (
+    ELNAnnotation,
+)
+from nomad.datamodel.metainfo.basesections import (
+    ActivityStep,
+    Entity,
+    SectionReference,
+)
 from nomad.metainfo import (
+    Quantity,
     SchemaPackage,
+    SubSection,
 )
 
 m_package = SchemaPackage()
+
+
+class AnalysisResult(Entity):
+    """
+    An abstract class representing the results of an analysis. The data model is
+    intended to be extended for specific analysis methods.
+    """
+
+
+class AnalysisResultReference(SectionReference):
+    """
+    A reference to a NOMAD AnalysisResult section.
+    """
+
+    reference = Quantity(
+        type=AnalysisResult,
+        description='A reference to a NOMAD AnalysisResult section.',
+        a_eln=ELNAnnotation(
+            component='ReferenceEditQuantity',
+            label='section reference',
+        ),
+    )
+
+
+class AnalysisStep(ActivityStep):
+    """
+    An abstract class representing the step of an analysis. It contains the results of
+    the analysis step. The data model is intended to be extended for specific analysis
+    methods.
+    """
+
+    results = SubSection(
+        section_def=AnalysisResultReference,
+        repeats=True,
+    )
 
 
 m_package.__init_metainfo__()
