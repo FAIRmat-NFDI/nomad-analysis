eae58ff753f42ebbe6ad3e63336b5fa2d3f4e45f
Sarthak Kapoor <sarthakkapoor@hotmail.com>
2024-09-25 00:39:34 +0200

    Add utils for API requests

--- src/nomad_analysis/utils.py
+++ src/nomad_analysis/utils.py
@@ -23,6 +23,15 @@ Utility functions for the analysis plugin.
 
 import importlib
 import inspect
+import json
+from typing import TYPE_CHECKING, Any
+
+import requests
+from nomad.client.api import Auth
+from nomad.datamodel import EntryArchive
+
+if TYPE_CHECKING:
+    from nomad.datamodel.data import MSection
 
 
 def category(category_name: str = None) -> callable:
@@ -94,3 +103,159 @@ def list_to_string(list_instance: list) -> str:
     for item in list_instance:
         string += item + '\n'
     return string
+
+
+def get_reference(upload_id: str, entry_id: str) -> str:
+    """
+    Returns the proxy value for referencing an entry.
+
+    Args:
+        upload_id (str): Upload ID of the upload in which the entry resides.
+        entry_id (str): Entry ID of the entry.
+
+    Returns:
+        str: Proxy value of the form '../uploads/{upload_id}/archive/{entry_id}'
+    """
+    return f'../uploads/{upload_id}/archive/{entry_id}'
+
+
+def create_entry_with_api(
+    section: 'MSection',
+    base_url: str,
+    upload_id: str,
+    file_name: str,
+    path: str = '',
+    **params,
+) -> str:
+    """
+    Uses the NOMAD API endpoint `/uploads/{upload_id}/raw/{path}` to create an entry
+    for the given NOMAD section. Returns the proxy value for referencing the entry.
+
+    Args:
+        section (MSection): The entry data sections to be used for entry creation.
+        base_url (str): Base URL of the NOMAD installation.
+        upload_id (str): Upload ID of the upload in which the entry needs to be created.
+        file_name (str): Name of the file to be created in the entry.
+        path (str, Optional): Path in the NOMAD upload where the entry will be created.
+        params (dict): Additional parameters for the request.
+
+    Returns:
+        str: proxy value of the form '../uploads/{upload_id}/archive/{entry_id}'
+    """
+    endpoint = base_url + f'/uploads/{upload_id}/raw/{path}'
+
+    params['file_name'] = file_name
+    if 'wait_for_processing' not in params:
+        params['wait_for_processing'] = True
+
+    if not isinstance(section, EntryArchive):
+        entry = EntryArchive(data=section)
+    else:
+        entry = section
+
+    response = put_nomad_request(
+        url=endpoint, json_dict=entry.m_to_dict(), params=params
+    )
+
+    reference = get_reference(
+        upload_id=response['processing']['entry']['upload_id'],
+        entry_id=response['processing']['entry']['entry_id'],
+    )
+
+    return reference
+
+
+def process_upload_with_api(base_url: str, upload_id: str):
+    """
+    Uses the NOMAD API endpoint `/uploads/{upload_id}/action/process` to manually
+    trigger processing of an upload.
+
+    Args:
+        base_url (str): Base URL of the NOMAD installation.
+        upload_id (str): Upload ID of the upload to be processed
+    """
+    endpoint = base_url + f'/uploads/{upload_id}/action/process'
+
+    post_nomad_request(url=endpoint)
+
+
+def put_nomad_request(
+    url: str,
+    data: Any = None,
+    json_dict: dict = None,
+    params: dict = None,
+    timeout: int = None,
+) -> json:
+    """
+    Sends a put request to the NOMAD API.
+
+    Args:
+        url (str): Endpoint of the API.
+        data (Any, optional): Dictionary, list of tuples, bytes, or file-like object to
+            send in the body of the `Request`.
+        json_dict (dict, optional): A JSON serializable Python object to send in the
+            body of the `Request`.
+        params (dict, optional): Parameters for the request.
+        timeout (int, optional): Timeout for the request in seconds.
+
+    Returns:
+        json: Response from the API in JSON serializable Python object.
+    """
+
+    headers = {**Auth().headers()}
+
+    print(f'Sending post request @ {url}')
+
+    response = requests.put(
+        url,
+        headers=headers,
+        json=json_dict,
+        params=params,
+        data=data,
+        timeout=timeout,
+    )
+
+    if not response.ok:
+        raise ValueError(f'Unexpected response {response.json()}')
+
+    return response.json()
+
+
+def post_nomad_request(
+    url: str,
+    data: Any = None,
+    json_dict: dict = None,
+    params: dict = None,
+    timeout: int = None,
+) -> json:
+    """
+    Sends a post request to the NOMAD API.
+
+    Args:
+        url (str): Endpoint of the API.
+        data (Any, optional): Dictionary, list of tuples, bytes, or file-like object to
+            send in the body of the `Request`.
+        json_dict (dict, optional): A JSON serializable Python object to send in the
+            body of the `Request`.
+        params (dict, optional): Parameters for the request.
+        timeout (int, optional): Timeout for the request in seconds.
+
+    Returns:
+        json: Response from the API in JSON serializable Python object.
+    """
+    headers = {**Auth().headers()}
+
+    print(f'Sending post request @ {url}')
+
+    response = requests.post(
+        url,
+        headers=headers,
+        json=json_dict,
+        params=params,
+        data=data,
+        timeout=timeout,
+    )
+    if not response.ok:
+        raise ValueError(f'Unexpected response {response.json()}')
+
+    return response.json()
