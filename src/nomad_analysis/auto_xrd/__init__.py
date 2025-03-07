f45bf6cb1c8c9f81d85a2541e271ce0a692574e9
Sarthak Kapoor <sarthakkapoor@hotmail.com>
2025-03-07 17:02:29 +0100

    Add entrypoints

--- src/nomad_analysis/auto_xrd/__init__.py
+++ src/nomad_analysis/auto_xrd/__init__.py
@@ -1,12 +1,30 @@
 from nomad.config.models.plugins import SchemaPackageEntryPoint
-from pydantic import Field
 
 
 class NewSchemaPackageEntryPoint(SchemaPackageEntryPoint):
-    parameter: int = Field(0, description='Custom configuration parameter')
-
     def load(self):
         from nomad_analysis.auto_xrd.auto_xrd import m_package
+
+        return m_package
+
+
+class XRDAnalaysisSchemaPackageEntryPoint(SchemaPackageEntryPoint):
+    def load(self):
+        from nomad_analysis.auto_xrd.auto_xrd_analysis import m_package
+
+        return m_package
+
+
+class AnalysisSchemaPackageEntryPoint(SchemaPackageEntryPoint):
+    def load(self):
+        from nomad_analysis.auto_xrd.analysis import m_package
+
+        return m_package
+
+
+class TrainingSchemaPackageEntryPoint(SchemaPackageEntryPoint):
+    def load(self):
+        from nomad_analysis.auto_xrd.training import m_package
 
         return m_package
 
@@ -15,18 +33,15 @@ auto_xrd = NewSchemaPackageEntryPoint(
     name='auto_xrd',
     description='New schema package entry point configuration.',
 )
-
-
-class XRDAnalaysisSchemaPackageEntryPoint(SchemaPackageEntryPoint):
-    parameter: int = Field(0, description='Custom configuration parameter')
-
-    def load(self):
-        from nomad_analysis.auto_xrd.auto_xrd_analysis import m_package
-
-        return m_package
-
-
 auto_xrd_analysis = XRDAnalaysisSchemaPackageEntryPoint(
     name='auto_xrd_analysis',
     description='New schema package entry point configuration.',
+)
+analysis_schema = AnalysisSchemaPackageEntryPoint(
+    name='Auto XRD Inference Schema',
+    description='Schema for performing Auto XRD analysis.',
+)
+training_schema = TrainingSchemaPackageEntryPoint(
+    name='Auto XRD Training Schema',
+    description='Schema for training Auto XRD models.',
 )
