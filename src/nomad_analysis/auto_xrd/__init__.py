0b76019bc84fb180859a084427f0da4469fcc959
Sarthak Kapoor <sarthakkapoor@hotmail.com>
2025-03-10 14:07:20 +0100

    Refactor plugin entry points

--- src/nomad_analysis/auto_xrd/__init__.py
+++ src/nomad_analysis/auto_xrd/__init__.py
@@ -1,47 +1,18 @@
 from nomad.config.models.plugins import SchemaPackageEntryPoint
 
 
-class NewSchemaPackageEntryPoint(SchemaPackageEntryPoint):
+class AutoXRDSchemaPackageEntryPoint(SchemaPackageEntryPoint):
+    """
+    Schema for training Auto XRD models and running Auto XRD analysis.
+    """
+
     def load(self):
-        from nomad_analysis.auto_xrd.auto_xrd import m_package
+        from nomad_analysis.auto_xrd.schema import m_package
 
         return m_package
 
 
-class XRDAnalaysisSchemaPackageEntryPoint(SchemaPackageEntryPoint):
-    def load(self):
-        from nomad_analysis.auto_xrd.auto_xrd_analysis import m_package
-
-        return m_package
-
-
-class AnalysisSchemaPackageEntryPoint(SchemaPackageEntryPoint):
-    def load(self):
-        from nomad_analysis.auto_xrd.analysis import m_package
-
-        return m_package
-
-
-class TrainingSchemaPackageEntryPoint(SchemaPackageEntryPoint):
-    def load(self):
-        from nomad_analysis.auto_xrd.training import m_package
-
-        return m_package
-
-
-auto_xrd = NewSchemaPackageEntryPoint(
-    name='auto_xrd',
-    description='New schema package entry point configuration.',
-)
-auto_xrd_analysis = XRDAnalaysisSchemaPackageEntryPoint(
-    name='auto_xrd_analysis',
-    description='New schema package entry point configuration.',
-)
-analysis_schema = AnalysisSchemaPackageEntryPoint(
-    name='Auto XRD Inference Schema',
-    description='Schema for performing Auto XRD analysis.',
-)
-training_schema = TrainingSchemaPackageEntryPoint(
-    name='Auto XRD Training Schema',
-    description='Schema for training Auto XRD models.',
+schema = AutoXRDSchemaPackageEntryPoint(
+    name='Auto XRD',
+    description='Schema for training Auto XRD models and running Auto XRD analysis.',
 )
