70f1bdd9dcc98df0e9c405a5cc4e187a04992ade
Sarthak Kapoor <sarthakkapoor@hotmail.com>
2025-03-20 14:39:17 +0100

    Ruff

--- src/nomad_analysis/jupyter/schema.py
+++ src/nomad_analysis/jupyter/schema.py
@@ -47,7 +47,6 @@ from nomad.datamodel.metainfo.annotations import (
     BrowserAnnotation,
     ELNAnnotation,
     ELNComponentEnum,
-    Filter,
     SectionProperties,
 )
 from nomad.datamodel.metainfo.basesections import (
