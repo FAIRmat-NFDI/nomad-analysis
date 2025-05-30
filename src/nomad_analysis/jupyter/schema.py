aa528f9c38dab60ec78d5d5b14c3b667971a903b
Sarthak Kapoor <57119427+ka-sarthak@users.noreply.github.com>
2025-05-30 13:56:26 +0200

    Run super normalization to ensure name is available (#70)
    * Run super normalization to ensure name is available
    
    * Docstring
    
    * Use mainfile if the name is not available

--- src/nomad_analysis/jupyter/schema.py
+++ src/nomad_analysis/jupyter/schema.py
@@ -367,7 +367,7 @@ class JupyterAnalysis(Analysis, EntryData):
         self, archive: 'EntryArchive', logger: 'BoundLogger'
     ) -> list:
         """
-        Writes the pre-defined Jupyter notebook cells based on the analysis type.
+        Writes the pre-defined Jupyter notebook cells.
 
         Args:
             archive (EntryArchive): The archive containing the section.
@@ -379,6 +379,10 @@ class JupyterAnalysis(Analysis, EntryData):
         user = 'Unknown user'
         if archive.metadata.main_author:
             user = archive.metadata.main_author.name
+        notebook_heading = self.name
+        if not notebook_heading:
+            notebook_heading = archive.metadata.mainfile.split('.')[0].replace('_', ' ')
+
         cells = []
 
         source = [
@@ -398,7 +402,7 @@ class JupyterAnalysis(Analysis, EntryData):
             '    line-height: 1.4em;\n',
             '    font-weight: 600;\n',
             '    padding: 30px 200px 0px 30px;"\n',
-            f'>{self.name}</h1>\n',
+            f'>{notebook_heading}</h1>\n',
             '<p style="font-size: 1.25em; font-style: italic; padding: 5px 200px 30px 30px;"\n',  # noqa: E501
             f'>{user}</p>\n',
             '</div>\n',
@@ -495,7 +499,8 @@ class JupyterAnalysis(Analysis, EntryData):
 
     def normalize(self, archive: 'EntryArchive', logger: 'BoundLogger'):
         """
-        Normalizes the input references.
+        Handles the behavior of action triggers and normalizes the input
+        references.
         """
         if self.trigger_generate_notebook:
             self.generate_notebook(archive, logger)
@@ -504,6 +509,9 @@ class JupyterAnalysis(Analysis, EntryData):
             self.inputs = []
             self.trigger_reset_inputs = False
         self.normalize_input_references(archive, logger)
+
+        # running super().normalize() at this point ensures the workflow section uses
+        # filtered self.inputs
         super().normalize(archive, logger)
 
 
